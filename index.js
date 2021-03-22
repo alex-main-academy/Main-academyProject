733c415fbdb91003aeef678b583e16f4da691b46
Alex <oleksandrilnytskyi@gmail.com>
2021-03-22 20:36:07 +0200

    learn JS

---
+++ index.js
@@ -0,0 +1,24 @@
+// Создайте страницу, которая отобразит сообщение «Я JavaScript!»
+alert('Я JavaScript!');
+
+
+/* Объявите две переменные: admin и name.
+   Запишите строку "Джон" в переменную name.
+   Скопируйте значение из переменной name в admin.
+   Выведите на экран значение admin, используя функцию alert (должна показать «Джон») */
+let admin;
+let AdminName;
+AdminName = 'John';
+admin = AdminName;
+alert(admin);
+
+/* Что выведет этот скрипт?
+let name = "Ilya";
+alert( `hello ${1}` ); 
+alert( `hello ${"name"}` ); 
+alert( `hello ${name}` ); 
+
+Скрипт выведет 3 алерта 
+1-й hello 1
+2-й hello name
+3-й hello Ilya*/
