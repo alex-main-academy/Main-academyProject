19a84fd3bd137ba0bf8808826d79d6af9901af65
Alex <oleksandrilnytskyi@gmail.com>
2021-03-30 17:09:36 +0300

    My HW

--- index.js
+++ index.js
@@ -1,26 +1,47 @@
-// Создайте страницу, которая спрашивает имя у пользователя и выводит его
+// Используя конструкцию if..else, напишите код, который получает число через prompt, а затем выводит в alert:
+// 1, если значение больше нуля,
+// -1, если значение меньше нуля,
+// 0, если значение равно нулю.
+// // Предполагается, что пользователь вводит только числа.
 
-let userName = prompt('What is your name?');
-alert('User Name: ' + userName);
+let num = Number(prompt('Your number: ',0));
+if (num > 0) {
+    alert(1)
+}else if (num < 0) {
+    alert(-1)
+}else{
+    alert(0)
+}
 
-// Ниже приведён код, который запрашивает у пользователя два числа и показывает их сумму.
-// Он работает неправильно. Код в примере выводит 12 (для значения полей по умолчанию).
-// В чём ошибка? Исправьте её. Результат должен быть 3
-// let a = prompt("Первое число?", 1);
-// let b = prompt("Второе число?", 2);
-// alert(a + b);
+// Перепишите конструкцию if с использованием условного оператора '?':
 
-let firstNum = prompt('first number? ', 1);
-let secondNum = prompt('second number?', 2);
+// let result;
 
-alert(Number(firstNum) + Number(secondNum));
+// if (a + b < 4) {
+//   result = 'Мало';
+// } else {
+//   result = 'Много';
+// }
 
-let firstNum = prompt('first number? ', 1);
-let secondNum = prompt('second number?', 2);
+result = (a + b < 4) ? 'Мало' : 'Много';
 
-alert(Number(firstNum) + Number(secondNum));
+// Перепишите if..else с использованием нескольких операторов '?'.
 
-let firstNum = prompt('first number? ', 1);
-let secondNum = prompt('second number?', 2);
+// Для читаемости рекомендуется разбить код на несколько строк.
 
-alert(Number(firstNum) + Number(secondNum));
+// let message;
+
+// if (login == 'Сотрудник') {
+//   message = 'Привет';
+// } else if (login == 'Директор') {
+//   message = 'Здравствуйте';
+// } else if (login == '') {
+//   message = 'Нет логина';
+// } else {
+//   message = '';
+// }
+
+let message = (login == 'Сотрудник') ? 'Привет' :
+  (login == 'Директор') ? 'Здравствуйте' :
+  (login == '') ? 'Нет логина' :
+  '';
